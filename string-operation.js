9752178fa2f571bd34ee4be312e66940057f37af
hnmahamud <hnmahamud@gmail.com>
2023-02-04 20:46:34 +0600

    string-operation

---
+++ string-operation.js
@@ -0,0 +1,8 @@
+function stringCombine (str1, str2) {
+    return str1 + " " + str2;
+}
+
+const firstString = "I am going to be";
+const secondString = "an awesome web developer";
+const result = stringCombine(firstString, secondString);
+console.log(result);
