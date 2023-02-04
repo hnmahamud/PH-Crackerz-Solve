8cd579cd96e1cd534f9e559f834cd31b20cb9375
hnmahamud <hnmahamud@gmail.com>
2023-02-05 00:20:58 +0600

    area-of-a-rectangle

---
+++ area-of-a-rectangle.js
@@ -0,0 +1,10 @@
+function findArea( length, width ) {
+    const recArea = length * width;
+    return recArea;
+    
+}
+
+const recLength = 10;
+const recWidth = 5;
+const result = findArea(recLength, recWidth);
+console.log(result);
